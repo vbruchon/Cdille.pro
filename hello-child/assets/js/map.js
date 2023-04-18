55aca1185257caf5e135c49bd3a17f96ca5f348e
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-04-18 14:14:55 +0200

    Refacto

--- hello-child/assets/js/map.js
+++ hello-child/assets/js/map.js
@@ -4,7 +4,7 @@ let mapUrl = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
 const map = L.map("map").setView([44.7241, 5.0864], 9);
 
 let iconCedille = L.icon({
-    iconUrl: "wp-content/themes/hello-elementor/assets/images/markerC.png",
+    iconUrl: "/wp-content/themes/hello-child/assets/images/markerC.png",
     iconSize: [65, 67]
 });
 
@@ -20,72 +20,157 @@ let allMarkersGroup = L.featureGroup().addTo(map);
 // Create feature group for filtered markers
 let filteredMarkersGroup = L.featureGroup().addTo(map);
 
+
+
 let markers = [];
 let list = document.getElementById("list");
 let form = document.getElementById("filter-form");
+let contentDiv = document.getElementById("content-div");
+let mapDiv = document.getElementById("map");
 
 
-fetch("wp-content/themes/hello-child/assets/geojson/newData.json")
+
+fetch("/wp-content/themes/hello-child/assets/geojson/newData.json")
     .then(response => response.text())
     .then(data => {
         const parsedData = JSON.parse(data); //parse data en JSON
-
         //Create a ClusterGroup to group markers
         let markerClusterGroup = L.markerClusterGroup();
 
-        for (let tiersLieux of parsedData.features) {
-            //content = object contains the tiers-lieux's content
-            let content = {
-                "lat": tiersLieux.geometry.coordinates[1],
-                "long": tiersLieux.geometry.coordinates[0],
-                "name": tiersLieux.properties.identification.nom_long,
-                "adress": {
-                    "street": tiersLieux.properties.coordonees.adresse,
-                    "code": tiersLieux.properties.coordonees.code_postal,
-                    "city": tiersLieux.properties.coordonees.commune,
-                },
-                "contact": {
-                    "tel": tiersLieux.properties.coordonees.telephone,
-                    "email": tiersLieux.properties.coordonees.mail,
-                    "webSite": tiersLieux.properties.coordonees.site_internet,
-                },
-                "socialMedia": {
-                    "url1": tiersLieux.properties.communication.reseau_social_1,
-                    "url2": tiersLieux.properties.communication.reseau_social_2,
-                    "url3": tiersLieux.properties.communication.reseau_social_3,
-                    "url4": tiersLieux.properties.communication.reseau_social_4,
-                },
-                "desc": tiersLieux.properties.complement_info.descriptif_court,
-                "accessibility": tiersLieux.properties.complement_info.accessibilite,
-            };
+        createForm(form, filteredMarkersGroup, allMarkersGroup, markers);
 
-            //Create the content of Popup
-            let popupContent = `<div class="name">${content.name}</div>`;
+        createMapAndListElement(markers, markerClusterGroup, parsedData);
 
-            //Create marker with lat and long; add custom icon and add popup
-            let marker = L.marker([content.lat, content.long], { icon: iconCedille });
-            marker.bindPopup(popupContent);
-
-            //Add marker to markerClusterGroup
-            markerClusterGroup.addLayer(marker);
-            //Add marker in markers array 
-            markers.push(marker);
-
-            /*            //Add marker to the map and add bindPopup
-                       let marker = L.marker([content.lat, content.long], { icon: iconCedille }).addTo(map);
-                       marker.bindPopup(popupContent);
-                       //Add marker in markers array 
-                       markers.push(marker); */
-
-
-            let item = createElementCard(content, map, marker)
-            list.appendChild(item);
-        }
-        //map.fitBounds(markerClusterGroup.getBounds());
-
+        map.fitBounds(markerClusterGroup.getBounds(), { minZoom: 9 });
         map.addLayer(markerClusterGroup);
-        //Create form
-        form.innerHTML += ` <input type="radio" id="all" name="type" value="all" checked>
+
+
+        const cards = document.querySelectorAll('.card');
+        cardsEvent(cards);
+
+        fixMapLoadingBugs(map)
+    })
+    .catch(error => console.error(error));
+
+//Stratégie pour la gestion d'affichage sur mobile.
+/* 
+Vérifier si on est sur mobile
+Créer deux boutons list et map
+si je clique sur le bouton map il affiche la map
+sinon il affiche la list
+Par défaut list afficher
+Voir pour la gestion de liements des card aux markers 
+*/
+
+
+
+
+
+
+
+
+
+
+function fixMapLoadingBugs(map) {
+    setInterval(function () {
+        map.invalidateSize();
+    }, 100);
+}
+
+
+
+function cardsEvent(cards) {
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+
+            let hiddenFields = {
+                "tel": card.querySelector('.tel'),
+                "email": card.querySelector('.email'),
+                "webSite": card.querySelector('.web-site'),
+                "social": card.querySelector('.social-media'),
+                "accessibility": card.querySelector('accessibility')
+            }
+            // Check if clicked card is already active
+            const isActive = card.classList.contains('active');
+
+            // Remove active class from all other cards 
+            cards.forEach(otherCard => {
+                if (otherCard !== card && otherCard.classList.contains('active')) {
+                    otherCard.classList.remove('active');
+                }
+            });
+
+            // Add or remove active class at clicked card
+            if (isActive) {
+                card.classList.remove('active');
+                for (let key in hiddenFields) {
+                    hiddenFields[key].classList.add('hidden')
+                }
+            } else {
+                card.classList.add('active');
+                for (let key in hiddenFields) {
+                    hiddenFields[key].classList.remove('hidden')
+                };
+            }
+        });
+    });
+}
+
+
+
+
+function createMapAndListElement(markers, markerClusterGroup, parsedData) {
+    for (let tiersLieux of parsedData.features) {
+        //content = object contains the tiers-lieux's content
+        let content = {
+            "lat": tiersLieux.geometry.coordinates[1],
+            "long": tiersLieux.geometry.coordinates[0],
+            "name": tiersLieux.properties.identification.nom_long,
+            "adress": {
+                "street": tiersLieux.properties.coordonees.adresse,
+                "code": tiersLieux.properties.coordonees.code_postal,
+                "city": tiersLieux.properties.coordonees.commune,
+            },
+            "contact": {
+                "tel": tiersLieux.properties.coordonees.telephone,
+                "email": tiersLieux.properties.coordonees.mail,
+                "webSite": tiersLieux.properties.coordonees.site_internet,
+            },
+            "socialMedia": {
+                "url1": tiersLieux.properties.communication.reseau_social_1,
+                "url2": tiersLieux.properties.communication.reseau_social_2,
+                "url3": tiersLieux.properties.communication.reseau_social_3,
+                "url4": tiersLieux.properties.communication.reseau_social_4,
+            },
+            "desc": tiersLieux.properties.complement_info.descriptif_court,
+            "accessibility": tiersLieux.properties.complement_info.accessibilite,
+        };
+
+        //Create the content of Popup
+        let popupContent = `<div class="name">${content.name}</div>`;
+
+        //Create marker with lat and long; add custom icon and add popup
+        let marker = L.marker([content.lat, content.long], { icon: iconCedille });
+        marker.bindPopup(popupContent);
+
+        //Add marker to markerClusterGroup
+        markerClusterGroup.addLayer(marker);
+        //Add marker in markers array 
+        markers.push(marker);
+
+        let item = createElementCard(content, map, marker)
+        list.appendChild(item);
+        marker.addEventListener("click", () => {
+            item.classList.add("active");
+        });
+    }
+}
+
+
+
+
+function createForm(form, filteredMarkersGroup, allMarkersGroup, markers) {
+    form.innerHTML += ` <input type="radio" id="all" name="type" value="all" checked>
                             <label for="all">Tous les Tiers-Lieux</label><br>
 
                             <input type="radio" id="5" name="type" value="5">
@@ -95,111 +180,70 @@ fetch("wp-content/themes/hello-child/assets/geojson/newData.json")
                             <label for="two">2 Tiers-Lieux</label><br>
         `;
 
-        form.addEventListener("change", (e) => {
-            let radios = form.elements.type;
-            let selectedType;
-            let numSelected;
+    form.addEventListener("change", (e) => {
+        let radios = form.elements.type;
+        let selectedType;
+        let numSelected;
 
-            for (let radio of radios) {
-                if (radio.checked) {
-                    selectedType = radio.value;
-                    break;
-                }
+        for (let radio of radios) {
+            if (radio.checked) {
+                selectedType = radio.value;
+                break;
+            }
+        }
+
+        // Determines the number of markers to display based on the selected value
+        if (selectedType === "all") {
+            numSelected = markers.length;
+
+            markers.forEach((marker) => {
+                allMarkersGroup.addLayer(marker);
+            })
+            filteredMarkersGroup = allMarkersGroup;
+        } else {
+            numSelected = parseInt(selectedType);
+
+            filteredMarkersGroup.clearLayers();
+
+            // Add filtered markers to the filteredMarkersGroup
+            for (var i = 0; i < numSelected; i++) {
+                filteredMarkersGroup.addLayer(markers[i]);
             }
 
-            // Determines the number of markers to display based on the selected value
-            if (selectedType === "all") {
-                numSelected = markers.length;
+            // Remove unfiltered markers from the allMarkersGroup
+            markers.forEach(function (marker) {
+                if (!filteredMarkersGroup.hasLayer(marker)) {
+                    allMarkersGroup.removeLayer(marker);
+                }
+            });
+        }
+        // Centering on the boundaries of the filtered markers
+        map.fitBounds(filteredMarkersGroup.getBounds());
 
-                markers.forEach((marker) => {
-                    allMarkersGroup.addLayer(marker);
-                })
-                filteredMarkersGroup = allMarkersGroup;
+
+        // Browse all markers and add or remove from the map incording to the value of numSelected
+        markers.forEach((marker, index) => {
+            if (index < numSelected) {
+                marker.addTo(map);
             } else {
-                numSelected = parseInt(selectedType);
-
-                filteredMarkersGroup.clearLayers();
-
-                // Add filtered markers to the filteredMarkersGroup
-                for (var i = 0; i < numSelected; i++) {
-                    filteredMarkersGroup.addLayer(markers[i]);
-                }
-
-                // Remove unfiltered markers from the allMarkersGroup
-                markers.forEach(function (marker) {
-                    if (!filteredMarkersGroup.hasLayer(marker)) {
-                        allMarkersGroup.removeLayer(marker);
-                    }
-                });
-            }
-            // Centering on the boundaries of the filtered markers
-            map.fitBounds(filteredMarkersGroup.getBounds());
-
-
-            // Browse all markers and add or remove from the map incording to the value of numSelected
-            markers.forEach((marker, index) => {
-                if (index < numSelected) {
-                    marker.addTo(map);
-                } else {
-                    marker.removeFrom(map);
-                }
-            });
-
-
-            // Display elements of the list incording to markers display 
-            const listItems = list.querySelectorAll(".card");
-
-            for (let i = 0; i < listItems.length; i++) {
-                if (i < numSelected) {
-                    listItems[i].style.display = "block";
-                } else {
-                    listItems[i].style.display = "none";
-                }
+                marker.removeFrom(map);
             }
         });
 
-        // Catch all .card
-        const cards = document.querySelectorAll('.card');
 
-        cards.forEach(card => {
-            card.addEventListener('click', () => {
-                
-                let hiddenFields = {
-                    "tel": card.querySelector('.tel'),
-                    "email": card.querySelector('.email'),
-                    "webSite": card.querySelector('.web-site'),
-                    "social": card.querySelector('.social-media'),
-                    "accessibility": card.querySelector('accessibility')
-                }
-                // Check if clicked card is already active
-                const isActive = card.classList.contains('active');
+        // Display elements of the list incording to markers display 
+        const listItems = list.querySelectorAll(".card");
 
-                // Remove active class from all other cards 
-                cards.forEach(otherCard => {
-                    if (otherCard !== card && otherCard.classList.contains('active')) {
-                        otherCard.classList.remove('active');
-                    }
-                });
+        for (let i = 0; i < listItems.length; i++) {
+            if (i < numSelected) {
+                listItems[i].style.display = "block";
+            } else {
+                listItems[i].style.display = "none";
+            }
+        }
+    });
 
-                // Add or remove active class at clicked card
-                if (isActive) {
-                    card.classList.remove('active');
-                    for(let key in hiddenFields){
-                        hiddenFields[key].classList.add('hidden')
-                    }
-                } else {
-                    card.classList.add('active');
-                    for(let key in hiddenFields){
-                        hiddenFields[key].classList.remove('hidden')
-                    };
-                }
-            });
-        });
-
-    })
-    .catch(error => console.error(error));
-
-
+}
 
 
 
@@ -228,13 +272,13 @@ function createElementCard(content, map, marker) {
     card.addEventListener("click", () => {
         marker.openPopup();
         map.setView(marker.getLatLng(), 9);
-
-/*         fields.tel.classList.remove("hidden");
-        fields.email.classList.remove("hidden");
-        fields.webSite.classList.remove("hidden");
-        fields.social.classList.remove("hidden");
-        fields.accessibility.classList.remove("hidden"); */
     });
+
+    /*         fields.tel.classList.remove("hidden");
+            fields.email.classList.remove("hidden");
+            fields.webSite.classList.remove("hidden");
+            fields.social.classList.remove("hidden");
+            fields.accessibility.classList.remove("hidden"); */
 
     return card;
 }
@@ -329,10 +373,10 @@ function createSocialMediaElement(content) {
     console.log(urlSocialMedia);
 
     let imgSocialMedia = {
-        "facebook": "wp-content/themes/hello-child/assets/images/facebook.png",
-        "instagram": "wp-content/themes/hello-child/assets/images/instagram.png",
-        "twitter": "wp-content/themes/hello-child/assets/images/twitter.png",
-        "linkedin": "wp-content/themes/hello-child/assets/images/linkedin.png"
+        "facebook": "/wp-content/themes/hello-child/assets/images/facebook.png",
+        "instagram": "/wp-content/themes/hello-child/assets/images/instagram.png",
+        "twitter": "/wp-content/themes/hello-child/assets/images/twitter.png",
+        "linkedin": "/wp-content/themes/hello-child/assets/images/linkedin.png"
     };
 
     for (let i = 0; i < urlSocialMedia.length; i++) {
