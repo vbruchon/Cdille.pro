eb2c6a53a5191f698058bb4ed921d95afc6b7045
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-04-28 12:38:11 +0200

    feat: add logique for serviceFilterBar

--- hello-child/assets/js/map.js
+++ hello-child/assets/js/map.js
@@ -44,11 +44,15 @@ function createDomElement(balise, idOrClass = "", content = "", innerHtml = "") 
  * @param {NodeList} listItems - A NodeList of list items
  * @param {Object} markerClusterGroup - The marker cluster group object
  */
-function filteredMapAndList(markers, typePlaceSelected, listItems, markerClusterGroup) {
+function filteredMapAndList(markers, typePlaceSelected, serviceSelected, listItems, markerClusterGroup) {
+    markerClusterGroup.clearLayers();
+
     markers.forEach((marker) => {
         const markerTypePlaces = marker.properties.typePlace;
+        const markerService = marker.properties.services;
 
-        if (typePlaceSelected.includes("all") || typePlaceSelected.some((type) => markerTypePlaces.includes(type))) {
+        if ((typePlaceSelected.includes("all") || typePlaceSelected.some((type) => markerTypePlaces.includes(type)))
+            && (serviceSelected.includes("all") || serviceSelected.some((type) => markerService.includes(type)))) {
             markerClusterGroup.addLayer(marker);
         }
     });
@@ -56,17 +60,16 @@ function filteredMapAndList(markers, typePlaceSelected, listItems, markerCluster
 
     listItems.forEach((card) => {
         let cardTypePlace = card.getElementsByClassName("type-place");
+        let cardService = card.getElementsByClassName("services");
 
-        if (typePlaceSelected.includes("all") || typePlaceSelected.some((type) => cardTypePlace[0].textContent.includes(type))) {
+        if ((typePlaceSelected.includes("all") || typePlaceSelected.some((type) => cardTypePlace[0].textContent.includes(type)))
+            && (serviceSelected.includes("all") || serviceSelected.some((type) => cardService[0].textContent.includes(type)))) {
             card.style.display = "block";
         } else {
             card.style.display = "none";
         }
     });
 }
-
-
-    
 /**
  * Creates a filter bar to select types of tiers-lieux and services.
  * @param {HTMLFormElement} form - The form element to add the filter bar to.
@@ -129,27 +132,38 @@ function createFilterBar(form, markers, parsedData, markerClusterGroup, map) {
         }
     }
 
-    typePlaceForm.addEventListener("change", () => {
-        const checkboxes = typePlaceForm.querySelectorAll('input[type="checkbox"]');
-        const listItems = list.querySelectorAll(".card");
-        const typePlaceSelected = [];
-        const all = document.querySelector('#typePlaceForm .select-items #all')
-
-        checkboxes.forEach(checkbox => {
-            if (checkbox.checked) {
-                typePlaceSelected.push(checkbox.value);
+    const options = document.querySelectorAll('.option');
+    options.forEach(option => {
+        option.addEventListener("click", () => {
+            let input = option.querySelector('input');
+            console.log(input);
+            if (input.checked) {
+                input.removeAttribute('checked');
+            } else {
+                input.setAttribute('checked', 'checked');
             }
-        })
-        /** 
-         * Si autre que all est checked
-         *  retirer le checked à all
-         * Si all est chequer 
-         *  retirer le chacked à toutes les autres.
-        */
+            const checkboxesTypePlace = typePlaceForm.querySelectorAll('#typePlaceForm input[type="checkbox"]');
+            const checkboxeServices = servicesForm.querySelectorAll('#servicesForm input[type="checkbox"]');
+            const listItems = list.querySelectorAll(".card");
 
-        markerClusterGroup.clearLayers();
-        filteredMapAndList(markers, typePlaceSelected, listItems, markerClusterGroup);
-    });
+            const typePlaceSelected = [];
+            const serviceSelected = [];
+
+            checkboxesTypePlace.forEach(checkbox => {
+                if (checkbox.checked) {
+                    typePlaceSelected.push(checkbox.value);
+                }
+            })
+
+            checkboxeServices.forEach(checkbox => {
+                if (checkbox.checked) {
+                    serviceSelected.push(checkbox.value);
+                }
+            });
+
+            filteredMapAndList(markers, typePlaceSelected, serviceSelected, listItems, markerClusterGroup)
+        })
+    })
 
     // Ouverture et fermeture de la liste déroulante typePlace
     const selectedTypePlace = document.querySelector('#typePlaceForm .select-selected');
@@ -172,33 +186,34 @@ function createFilterBar(form, markers, parsedData, markerClusterGroup, map) {
         }
     });
 }
+
 /**
  * Create a list of elements and markers on the map
  * @param {L.markerClusterGroup} markerClusterGroup - The marker group for the map
-* @param {Object} parsedData - The parsed data for the tiers
-*
-* The content object contains all the information about a location
-* @typedef {Object} content
-* @property {number} lat – The latitude of the location
-* @property {number} long – The longitude of the location
-* @property {string} name – The name of the location
-* @property {Object} address – The address of the location
-* @property {string} address.street - The street of the location
-* @property {string} adress.code - The postal code of the Offsite
-* @property {string} adress.city - The city of the co-location
-* @property {Object} contact – The contact information of the shop
-* @property {string} contact.tel - The phone number of the shop
-* @property {string} contact.email - The email address of the shop
-* @property {string} contact.webSite - The website of the shop
-* @property {Object} socialMedia - The links to the social networks of the shop
-* @property {string} socialMedia.url1 – The first social network link
-* @property {string} socialMedia.url2 – The second social network link
-* @property {string} socialMedia.url3 – The third social network link
-* @property {string} socialMedia.url4 – The fourth social network link
-* @property {string} desc - The short description of the third place
-* @property {string} accessibility – The accessibility of the third-party venue
-* @property {string} typePlace - The type of the location
-*/
+ * @param {Object} parsedData - The parsed data for the tiers
+ *
+ * The content object contains all the information about a location
+ * @typedef {Object} content
+ * @property {number} lat – The latitude of the location
+ * @property {number} long – The longitude of the location
+ * @property {string} name – The name of the location
+ * @property {Object} address – The address of the location
+ * @property {string} address.street - The street of the location
+ * @property {string} adress.code - The postal code of the Offsite
+ * @property {string} adress.city - The city of the co-location
+ * @property {Object} contact – The contact information of the shop
+ * @property {string} contact.tel - The phone number of the shop
+ * @property {string} contact.email - The email address of the shop
+ * @property {string} contact.webSite - The website of the shop
+ * @property {Object} socialMedia - The links to the social networks of the shop
+ * @property {string} socialMedia.url1 – The first social network link
+ * @property {string} socialMedia.url2 – The second social network link
+ * @property {string} socialMedia.url3 – The third social network link
+ * @property {string} socialMedia.url4 – The fourth social network link
+ * @property {string} desc - The short description of the third place
+ * @property {string} accessibility – The accessibility of the third-party venue
+ * @property {string} typePlace - The type of the location
+ */
 function createListAndMapElement(markerClusterGroup, parsedData) {
     for (let tiersLieux of parsedData.features) {
         //content = object contains the tiers-lieux's content
@@ -224,14 +239,15 @@ function createListAndMapElement(markerClusterGroup, parsedData) {
             },
             "desc": tiersLieux.properties.complement_info.descriptif_court,
             "accessibility": tiersLieux.properties.complement_info.accessibilite,
-            "typePlace": tiersLieux.properties.complement_info.typePlace
+            "typePlace": tiersLieux.properties.complement_info.typePlace,
+            "services": tiersLieux.properties.complement_info.services
         };
 
         //Create the content of Popup
         let popupContent = `<div class="name">${content.name}</div>`;
         //Create marker with lat and long; add custom icon and add popup
         let marker = L.marker([content.lat, content.long], { icon: iconCedille });
-        marker.properties = { typePlace: content.typePlace }
+        marker.properties = { typePlace: content.typePlace, services: content.services }
         marker.bindPopup(popupContent);
 
         //Add marker to markerClusterGroup
@@ -267,7 +283,8 @@ function createElementCard(content, map, marker) {
         "email": createEmailElement(content),
         "webSite": createWebSiteElement(content),
         "social": createSocialMediaElement(content),
-        "typePlace": createTypePlaceElement(content)
+        "typePlace": createTypePlaceElement(content),
+        "services": createServicesElement(content)
     };
     let keyName = ["name", "adress"];
 
@@ -389,6 +406,15 @@ function createTypePlaceElement(content) {
     return typePlace;
 }
 
+function createServicesElement(content) {
+    let services = createDomElement("div", "services");
+    let servicesText = createDomElement("p", "", content.services);
+
+    services.appendChild(servicesText);
+    services.style.display = "none"
+    return services;
+}
+
 //__________EVENTS__________
 /**
  * Add click event listeners to each card element in the provided array
@@ -409,8 +435,7 @@ function cardEvent(cards) {
                 }
             });
             toggleActiveCard(card);
-            marker.openPopup();
-            map.setView(marker.getLatLng(), 12);
+
         });
     });
 }
@@ -422,8 +447,8 @@ function cardEvent(cards) {
  * Inserts the corresponding card at the beginning of the list.
  *
  * @param {L.Marker} marker - The marker to add the event listener to.
-        * @param {HTMLElement} item - The corresponding card element to activate or deactivate.
-        */
+ * @param {HTMLElement} item - The corresponding card element to activate or deactivate.
+ */
 function markerEvent(marker, item) {
     marker.addEventListener("click", () => {
         let allCards = document.querySelectorAll('.card');
@@ -443,7 +468,7 @@ function markerEvent(marker, item) {
 /**
  * Toggles the active state of a card and hides or shows its "info" element
  * @param {HTMLElement} card - The card element to toggle the active state and info visibility for
-        */
+ */
 function toggleActiveCard(card) {
     const isActive = card.classList.contains('active');
     let info = card.querySelector(".info");
@@ -469,17 +494,15 @@ L.tileLayer(mapUrl, { maxZoom: 19, attribution: "&copy; <a href=\"http://www.ope
 
 let markers = [];
 let buttonsCreated = false;
+const jsonData = "/wp-content/themes/hello-child/assets/geojson/newData.json";
 
 //DOM ELEMENT
 let list = document.getElementById("list");
 let form = document.getElementById("filter-form");
 let contentDiv = document.getElementById("content-div");
 let mapDiv = document.getElementById("map");
-//_________________________________________________________________________________________________________________________________________________________
 
-
-
-fetch("/wp-content/themes/hello-child/assets/geojson/newData.json")
+fetch(jsonData)
     .then(response => response.text())
     .then(data => {
         const parsedData = JSON.parse(data);
@@ -492,3 +515,14 @@ fetch("/wp-content/themes/hello-child/assets/geojson/newData.json")
         cardEvent(document.querySelectorAll('.card'));
     })
     .catch(error => console.error(error));
+
+
+
+
+/**
+ * Quand j'arrive sur la page dans les deux formulaires all est checked.
+ * 
+ * J'écoute les changements sur typeplaceform et servicesform
+ * Si l'
+ */
+
